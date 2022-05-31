e1f949f2f4e121726e0bef56fc6be732b46f5228
Annie1521 <79653792+Annie1521@users.noreply.github.com>
2022-05-31 11:23:28 -0400

    nn

--- Tuck.js
+++ Tuck.js
@@ -40,21 +40,61 @@ var purchases={
         Wednesday:["sweater", "candy"],
     },
 }
-
-function findTotal(){
-    var items=purchases.Annie.Monday
-    var pricer=0;
+var pricer
+var taxedPrice
+function findTotal(name,day){
+    pricer=0
+    var item=purchases[name]
+    var items=item[day]
     
-    console.log(products.items[0])
+    
+    
     for(i=0;i<items.length;i++){
-        
+        console.log(items[i])
         var thing=items[i]
-        var pricey=products.thing
-        alert(pricey)
+        var thingy=window[thing]
+        console.log(thing+thingy)
+        var pricey=parseInt(products[items[i]].price)
+        // alert(pricey)
         pricer=pricer+pricey
         
     }
-    var taxedPrice=pricer*1.13
-    alert(price,taxedPrice)
+    taxedPrice=pricer*1.13
+
+    
+    console.log(Object.keys(purchases))
 }
-findTotal()
+findTotal("Annie","Monday")
+
+function Review(name,day){
+    var item=purchases[name]
+    var items=item[day]
+    var div1=document.createElement('div')
+    document.body.appendChild(div1)
+    div1.innerHTML="Reciept for: "+name+"      Date: "+day
+    var div2=document.createElement('div')
+    document.body.appendChild(div2)
+    div2.innerHTML=products[items[0]].name+"       $"+products[items[0]].price
+    var div3=document.createElement('div')
+    document.body.appendChild(div3)
+    div3.innerHTML=products[items[1]].name+"       $"+products[items[1]].price
+    var div4=document.createElement('div')
+    document.body.appendChild(div4)
+    div4.innerHTML="Total: $"+pricer
+    var div5=document.createElement('div')
+    document.body.appendChild(div5)
+    div5.innerHTML="Taxed Total: $"+Math.round(taxedPrice*100)/100
+
+}
+Review("Annie","Monday")
+
+function EndOfDay(day){
+    var endTotal=0
+    var People=Object.keys(purchases)
+    for(let i=0;i<People.length;i++){
+        findTotal(People[i],day)
+        endTotal=endTotal+pricer
+    }
+    alert("The end of day total is $"+endTotal)
+}
+EndOfDay("Monday")
---
+++ objects.js
@@ -0,0 +1,42 @@
+
+var products={
+    pencil:{
+        price: 2,
+        name:"pencil",
+    },
+    chips:{
+        price: 3,
+        name:"chips",
+    },
+    candy:{
+        price: 4,
+        name:"candy",
+    },
+    sweater:{
+        price: 5,
+        name:"sweater",
+    }
+}
+
+var purchases={
+    Annie:{
+        Monaday:[chips, candy],
+        Tuesday:[sweater, chips],
+        Wednesday:[candy, pencil],
+    },
+    Jazz:{
+        Monaday:[pencil, chips],
+        Tuesday:[candy, pencil],
+        Wednesday:[chips, caandy],
+    },
+    Virgil:{
+        Monaday:[chips, sweater],
+        Tuesday:[pencil, candy],
+        Wednesday:[candy, chips],
+    },
+    Kailah:{
+        Monaday:[pencil, sweater],
+        Tuesday:[chips, pencil],
+        Wednesday:[sweater, candy],
+    },
+}
